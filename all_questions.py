fd210087a8de8f22c3e94bfea47acd8a17fcd8b0
TallyGreen <127121815+TallyGreen@users.noreply.github.com>
2024-03-31 11:17:24 -0400

    Update all_questions.py

--- all_questions.py
+++ all_questions.py
@@ -10,7 +10,7 @@ def question1():
     # string "yes" or "no"
     answers["(a)"] = "no"
     answers["(b)"] = "no"
-    answers["(c)"] = "no"
+    answers["(c)"] = "yes"
     answers["(d)"] = "yes"
 
     # explain-string: explanation in english prose
@@ -48,12 +48,12 @@ def question3():
     # string "yes" or "no"
     answers["(a)"] = "no"
     answers["(b)"] = "no"
-    answers["(c)"] = None
+    answers["(c)"] = "Yes"
 
     # explain-string: explanation in english prose
     answers["(a) example"] = "pigeon will trigger R1 and R3"
-    answers["(b) example"] = "some record "
-    answers["(c) example"] = None
+    answers["(b) example"] = "some record like leopard "
+    answers["(c) example"] = "because the pigeon will trigger two rules and get two different results"
 
     return answers
 # -----------------------------------------------------------
@@ -61,51 +61,50 @@ def question7():
     answers = {}
 
     # bool: True/False
-    answers["(a)"] = None
-    answers["(b)"] = None
-    answers["(c)"] = None
-    answers["(d)"] = None
+    answers["(a)"] = False
+    answers["(b)"] = True
+    answers["(c)"] = False
+    answers["(d)"] = False
 
     # explain_string: explanation in english prose
-    answers["(a) explain"] = None
-    answers["(b) explain"] = None
-    answers["(c) explain"] = None
-    answers["(d) explain"] = None
+    answers["(a) explain"] = "In back-propagationit, k-1 layer information will be used to cacualte k layr information"
+    answers["(b) explain"] = "ANN model will calculate in a forward way. In this way, the activation in the k-1 layer will be weighted and used as the input in the k layer"
+    answers["(c) explain"] = "Gradients of the loss function are influenced by the input value and become very small. The weight of the layers will become small"
+    answers["(d) explain"] = "Loss function and the activation functions still have gradients, it will not be zero "
 
     return answers
-
 # -----------------------------------------------------------
 def question8():
     answers = {}
 
     # float
-    answers["(a) P(X_1=1)"] = None
-    answers["(a) P(X_2=1)"] = None
-    answers["(a) P(X_1=1,X_2=1)"] = None
+    answers["(a) P(X_1=1)"] = 0.65
+    answers["(a) P(X_2=1)"] = 0.41
+    answers["(a) P(X_1=1,X_2=1)"] = 0.28
 
     # string: "dependent" or "independent"
-    answers["(a) Relationship between X_1 and X_2"] = None
+    answers["(a) Relationship between X_1 and X_2"] = "dependent"
 
     # string: "yes" or "no"
-    answers["(b) X_1 and X_2 conditionally independent given the class?"] = None
+    answers["(b) X_1 and X_2 conditionally independent given the class?"] = "yes"
 
     # float
-    answers["(c) P(X_1=1 | +)"] = None
-    answers["(c) P(X_1=1 | -)"] = None
-    answers["(c) P(X_2=1 | +)"] = None
-    answers["(c) P(X_2=1 | -)"] = None
-    answers["(c) P(X_3=1 | +)"] = None
-    answers["(c) P(X_3=1 | -)"] = None
+    answers["(c) P(X_1=1 | +)"] = 0.8
+    answers["(c) P(X_1=1 | -)"] = 0.5
+    answers["(c) P(X_2=1 | +)"] = 0.5
+    answers["(c) P(X_2=1 | -)"] = 0.32
+    answers["(c) P(X_3=1 | +)"] = 0.4
+    answers["(c) P(X_3=1 | -)"] = 0.16
 
     # For each row give the class predicted by the model after training using Naive Bayes
     # String: either '+' or '-'
-    answers["(d) Row 1"] = None
-    answers["(d) Row 2"] = None
-    answers["(d) Row 3"] = None
-    answers["(d) Row 4"] = None
+    answers["(d) Row 1"] = "+"
+    answers["(d) Row 2"] = "-"
+    answers["(d) Row 3"] = "-"
+    answers["(d) Row 4"] = "-"
 
     # float between 0 and 1
-    answers["(d) Training error rate"] = None
+    answers["(d) Training error rate"] = 0.38
 
     return answers
 
@@ -114,12 +113,12 @@ def question9():
     answers = {}
 
     # int
-    answers["(a) K"] = None
-    answers["(b) K"] = None
+    answers["(a) K"] = 1
+    answers["(b) K"] = 5
 
     # explain_string
-    answers["(a) explain"] = None
-    answers["(b) explain"] = None
+    answers["(a) explain"] = "Two sets of data points are separated, so we just need K=1 to find the label"
+    answers["(b) explain"] = "Two sets of data points show overlaps. SO K value should be imporved. But K =50 will increase to much cost in coputation."
 
     return answers
 
@@ -128,54 +127,54 @@ def question10():
     answers = {}
 
     # float
-    answers["(a) P(A=1|+)"] = None
-    answers["(a) P(B=1|+)"] = None
-    answers["(a) P(C=1|+)"] = None
-    answers["(a) P(A=1|-)"] = None
-    answers["(a) P(B=1|-)"] = None
-    answers["(a) P(C=1|-)"] = None
+    answers["(a) P(A=1|+)"] = 0.6
+    answers["(a) P(B=1|+)"] = 0.4
+    answers["(a) P(C=1|+)"] = 0.8
+    answers["(a) P(A=1|-)"] = 0.4
+    answers["(a) P(B=1|-)"] = 0.4
+    answers["(a) P(C=1|-)"] = 0.2
 
     # type: explanatory string
-    answers["(a) P(A=1|+) explain your answer"] = None
+    answers["(a) P(A=1|+) explain your answer"] = "there are 3 A=1 in the positive set, there are 5 positive cases and 5 negative cases in the whole dataset"
   
     # type: float
     # note: R is the sample (A=1,B=1,C=1)
-    answers["(b) P(+|R)"] = None 
-    answers["(b) P(R|+)"] = None
-    answers["(b) P(R|-)"] = None
+    answers["(b) P(+|R)"] = 0.86
+    answers["(b) P(R|+)"] = 0.192
+    answers["(b) P(R|-)"] = 0.032
 
     # string, '+' or '-'
-    answers["(b) class label"] = None
+    answers["(b) class label"] = "+"
 
     # explain_string
-    answers["(b) Explain your reasoning"] = None
+    answers["(b) Explain your reasoning"] = "we can get the different conditinal probabilities for different value of ABC, and we have the prior probabilities which are 0.5 for+ 0.5 for -, then we use the formula to calclulate the p(R|+) and p(R|-), then we can get the p(+|R) and p(-|R), which one is bigger, the label wil be that"
   
     # float
-    answers["(c) P(A=1)"] = None
-    answers["(c) P(B=1)"] = None
-    answers["(c) P(A=1,B=1)"] = None
+    answers["(c) P(A=1)"] = 0.5
+    answers["(c) P(B=1)"] = 0.4
+    answers["(c) P(A=1,B=1)"] = 0.2
 
     # type: string, 'yes' or 'no'
-    answers["(c) A independent of B?"] = None
+    answers["(c) A independent of B?"] = "yes"
   
     # type: float
-    answers["(d) P(A=1)"] = None
-    answers["(d) P(B=0)"] = None
-    answers["(d) P(A=1,B=0)"] = None
+    answers["(d) P(A=1)"] = 0.5
+    answers["(d) P(B=0)"] = 0.6
+    answers["(d) P(A=1,B=0)"] = 0.3
 
     # type: string: 'yes' or 'no'
-    answers["(d) A independent of B?"] = None
+    answers["(d) A independent of B?"] = "yes"
   
     # type: float
-    answers["(e) P(A=1,B=1|+)"] = None
-    answers["(e) P(A=1|+)"] = None
-    answers["(e) P(B=1|+)"] = None
+    answers["(e) P(A=1,B=1|+)"] = 0.2
+    answers["(e) P(A=1|+)"] = 0.6
+    answers["(e) P(B=1|+)"] = 0.4
 
     # type: string: 'yes' or 'no'
-    answers["(e) A independent of B given class +?"] = None
+    answers["(e) A independent of B given class +?"] = "no"
 
     # type: explanatory string
-    answers["(e) A and B conditionally independent given class +, explain"] =  None
+    answers["(e) A and B conditionally independent given class +, explain"] =  "P(A=1,B=1|+)not equal to P(A=1|+)*P(B=1|+) "
   
     return answers
 # --------------------------------------------------------
